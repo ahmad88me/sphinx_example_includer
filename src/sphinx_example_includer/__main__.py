b7ad8716472024bb9b630f3dcfaaaa7bbc33bba0
Ahmad Alobaid <ahmad88me@gmail.com>
2024-08-08 21:01:56 +0200

    fix bugs

--- src/sphinx_example_includer/__main__.py
+++ src/sphinx_example_includer/__main__.py
@@ -1,6 +1,7 @@
 import argparse
-import includer
 import logging
+import os
+from .includer import generate_examples_rsts, generate_toc_rst
 
 
 def cli():
@@ -8,19 +9,27 @@ def cli():
         description='A tool to include example code or any other files into sphinx documentation automatically')
 
     parser.add_argument('--debug', action='store_true', help="Showing debug messages")
+    parser.add_argument('--overwrite', action='store_true', help="Overwrite files that already exists")
+
 
     parser.add_argument(
         '--files',
         nargs='+',
         required=True,
-        type=argparse.FileType('r'),
+        # type=argparse.FileType('r'),
         help='one or more files to be '
     )
 
     parser.add_argument(
         '--dest_dir',
-        default="docs/sources/examples",
+        default="docs/source/examples",
         help='The output directory'
+    )
+
+    parser.add_argument(
+        '--toc-fname',
+        default="examples_test.rst",
+        help='The name of the toc file'
     )
 
     args = parser.parse_args()
@@ -33,7 +42,12 @@ def cli():
     else:
         logger = None
     args = parser.parse_args()
-    includer.generate_examples_rsts(dest_dir=args.dest_dir, examples=args.files, logger=logger)
+    rst_files = generate_examples_rsts(dest_dir=args.dest_dir, examples=args.files, logger=logger, overwrite=args.overwrite)
+
+    parent_tokens = args.dest_dir.split(os.path.sep)[:-1]
+    toc_parent_path = os.path.sep.join(parent_tokens)
+    generate_toc_rst(toc_fname=args.toc_fname, examples_paths=rst_files, toc_parent_path=toc_parent_path,
+                     overwrite=args.overwrite, logger=logger)
 
 
 if __name__ == '__main__':
--- src/sphinx_example_includer/includer.py
+++ src/sphinx_example_includer/includer.py
@@ -1,5 +1,6 @@
 import os
 import traceback
+import logging
 
 
 def split_fname_ext(fpath):
@@ -11,40 +12,89 @@ def split_fname_ext(fpath):
     return fname, ext
 
 
+def get_logger():
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.WARN)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.WARN)
+    logger.addHandler(ch)
+    return logger
+
+
 def get_name_from_fname(fname):
     s = fname.replace("_", " ").replace("-", " ")
     return s.title()
 
 
-def generate_examples_rsts(examples, dest_dir, logger=None):
+def generate_examples_rsts(examples, dest_dir, logger=None, overwrite=False):
+    if logger is None:
+        logger = get_logger()
+    rst_ex_paths = []
     if not os.path.exists(dest_dir):
         try:
             os.mkdir(dest_dir)
         except Exception as e:
-            if logger:
-                logger.debug(traceback.format_exc())
-                logger.error(f"Exception: {e}")
-            else:
-                print(f"Exception: {e}")
-            return
-
+            logger.debug(traceback.format_exc())
+            logger.error(f"Exception: {e}")
+            return rst_ex_paths
     for exfile in examples:
         fname, ext = split_fname_ext(exfile)
         rst_filename = f'{fname}.rst'
         example_name = get_name_from_fname(fname)
         with open(exfile, 'r') as example_file:
             code = example_file.read()
+        code = code.strip()
+        icode = ""
+        for line in code.split("\n"):
+            icode += f"\t{line}\n"
         rst_content = f"""
 {example_name}
 {'=' * len(example_name)}
 
 .. code-block:: python
 
-{code}
+{icode}
 
 """
-        with open(os.path.join(dest_dir, rst_filename), 'w') as rst_file:
-            rst_file.write(rst_content)
-        if logger:
+        rst_path = os.path.join(dest_dir, rst_filename)
+        rst_ex_paths.append(rst_path)
+        if os.path.exists(rst_path) and not overwrite:
+            logger.warning(f"{rst_path} already exists and won't be overwritten")
+        else:
+            with open(rst_path, 'w') as rst_file:
+                rst_file.write(rst_content)
             logger.debug(f"Generated the docs {rst_filename}")
-    print(".rst files are generated successfully.")
+    logger.debug(".rst files are generated successfully.")
+    return rst_ex_paths
+
+
+def generate_toc_rst(toc_fname, examples_paths, toc_parent_path, logger=None, overwrite=False):
+    if logger is None:
+        logger = get_logger()
+    toc_name, _ = split_fname_ext(toc_fname)
+    example_name = get_name_from_fname(toc_name)
+    examples_txt = ""
+    for expath in examples_paths:
+        tokens = expath.split(os.path.sep)
+        ex_rel_path = os.path.sep.join(tokens[-2:])
+        examples_txt += f"\t{ex_rel_path}\n"
+
+    rst_content = f""".. _{toc_name}:
+
+{example_name}
+{'=' * len(example_name)}
+
+.. toctree::
+   :maxdepth: 1
+   :glob:
+
+{examples_txt}
+
+"""
+    rst_path = os.path.join(toc_parent_path, toc_fname)
+    if os.path.exists(rst_path) and not overwrite:
+        logger.warning(f"{rst_path} already exists and won't be overwritten.")
+    else:
+        with open(rst_path, 'w') as rst_file:
+            rst_file.write(rst_content)
+        logger.debug(f"Generated the toc {rst_path} successfully!")
